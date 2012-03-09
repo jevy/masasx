c519a74baed8b13e4fa3fbb77c4cde12af12de4a
lucapette <lucapette@gmail.com>
2012-03-09 10:52:11 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Masasx::Application.config.secret_token = '5d6a2047584996d2ef7d6bab403b32b4c4679ada5e2eb4c94d332428a92f2fc3b417c5f05877f79c261d0ae8d9386e0ed3375ed4d5591a986f51a3e197977514'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Masasx::Application.config.session_store :cookie_store, key: '_masasx_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Masasx::Application.config.session_store :active_record_store
