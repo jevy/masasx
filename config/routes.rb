bf44c7f45539b1210dc8d6eed8e527fbf7d01702
lucapette <lucapette@gmail.com>
2012-03-14 16:56:06 +0100

    Complete accept agreement steps

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Masasx::Application.routes.draw do
   get '/registration' => 'registrations#agreement'
 
+  post '/accept_agreements' => 'registrations#accept_agreements'
+
   root to: 'registrations#agreement'
 end
--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -3,9 +3,9 @@ Given /^I am on the signup page$/ do
 end
 
 Given /^I complete the agreement page$/ do
-  check('#agreement')
-  check('#media')
-  check('#community')
+  check('I accept the MASAS Information eXchange (MASAS-X) Pilot Participation Agreement.')
+  check('I am not a member of the media.')
+  check('I certify that I am a bona fide member of the Canadian Emergency Management community and have a valid reason to join the MASAS-X community.')
 end
 
 Given /^I follow (\w+)$/ do |label|
