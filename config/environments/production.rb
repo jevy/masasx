f41eefedd68d7b70974ccc75897762266785b8f7
Kuba Kuźma <kuba@jah.pl>
2014-04-04 16:23:08 +0200

    updated sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,9 @@ Masasx::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.default_url_options = {host: "masasx.heroku.com"}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
     address:              "smtp.sendgrid.net",
     port:                 "587",
     authentication:       :plain,
