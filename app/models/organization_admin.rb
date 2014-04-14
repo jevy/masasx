6012061328241830edf9ba42efca28d96ab07d43
Jevin Maltais <jevyjevjevs@gmail.com>
2014-04-14 11:31:34 -0400

    Allowing for non-unique emails

--- app/models/organization_admin.rb
+++ app/models/organization_admin.rb
@@ -1,12 +1,10 @@
 class OrganizationAdmin < ActiveRecord::Base
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable
-
   attr_accessible :email, :password, :password_confirmation, :remember_me, :role, :first_name, :last_name, :office_phone, :mobile_phone, :title, :language
   attr_accessible :address_line_1, :address_line_2, :city, :state, :country, :postal_code, :uuid
 
   belongs_to :organization
 
-  validates :email, presence: { message: 'Email address required' }, email: true, uniqueness: true
+  validates :email, presence: { message: 'Email address required' }, email: true
   validates :office_phone, presence: { message: 'Phone required' }
   validates :first_name, presence: { message: 'Name required' }
   validates :last_name, presence: { message: 'Name required' }
